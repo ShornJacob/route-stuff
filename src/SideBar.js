dd6fddc1d6d1dd4d4c0e6eab18a63e6684d1dce7
Shorn Jacob <shorn@transparency.consulting>
2019-04-15 12:21:01 +1200

    Added comments

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
+//https://reacttraining.com/react-router/web/guides/testing
 export default function SideBar({users}) {
   return (
     <div>
  
           <ul>
+              {/* map returns an from another array */}
               {users.map( user => (
                   <li>
                       <Link to={user.path}>{user.name}</Link>
