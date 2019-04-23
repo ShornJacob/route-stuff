2b3a960f62b65003ee4819322f2157ac7fe4c593
Shorn Jacob <shorn@transparency.consulting>
2019-04-23 14:56:01 +1200

    Route testing using one method 1

--- src/App3.test.js
+++ src/App3.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Bar from './Bar'
 import Jaaz from './Jaaz'
+import Routes from './Routes'
+import { Route } from 'react-router-dom';
+import A from './A'
+import Nomatch from './NoMatch'
+import NoMatch from './NoMatch';
 
 //https://techdoma.in/testing-routes-react-router-dom-in-react-using-jest-1/
 
@@ -13,24 +18,34 @@ describe('routes using array of routers', () => {
     //https://jestjs.io/docs/en/setup-teardown#one-time-setup
     //beforeAll is a onetime setup
 
-    let pathMap = {};
+    beforeAll(() => {
 
-    beforeAll( () => {
-        
 
         //https://airbnb.io/enzyme/docs/api/ReactWrapper/find.html#findselector--reactwrapper
         //.find(selector) => ReactWrapper
         //Finds every node in the render tree of the current wrapper that matches the provided selector.
 
+        const component = shallow(<Routes />);
 
-       
-  
-      })
+        //reduce will take each route, initialvalue of pathMap is also passed as {}
+        pathMap = component.find(Route).reduce((pathMap, route) => {
+            //the props of rach route
+            //eg { exact: true, path: '/b', component: [Function: B] }
+            const routeProps = route.props();
 
-      //https://airbnb.io/enzyme/docs/api/ShallowWrapper/reduce.html#returns
-      test('tesing reduce', () => {
+            //adding to pathMap Object
+            pathMap[routeProps.path] = routeProps.component;
 
-         //https://airbnb.io/enzyme/docs/api/ShallowWrapper/reduce.html#reducefn-initialvalue--any
+            return pathMap;
+        }, {})
+
+
+    })
+
+    //https://airbnb.io/enzyme/docs/api/ShallowWrapper/reduce.html#returns
+    test('tesing reduce', () => {
+
+        //https://airbnb.io/enzyme/docs/api/ShallowWrapper/reduce.html#reducefn-initialvalue--any
         //.reduce(fn[, initialValue]) => Any
         //Applies the provided reducing function to every node in the wrapper to reduce to a single value.
         // Each node is passed in as a ShallowWrapper, and is processed from left to right.
@@ -42,9 +57,18 @@ describe('routes using array of routers', () => {
 
         //        //(amount, n) => amount + n.prop('Jaaz') is the pased in fuction, 0 is initial value
 
-        const total = wrapper.find(Jaaz).reduce((amount, n) => amount + n.prop('amount'),0)
+        const total = wrapper.find(Jaaz).reduce((amount, n) => amount + n.prop('amount'), 0)
 
-        expect(total).toEqual(14); 
-          });
-
+        expect(total).toEqual(14);
     })
+
+
+    test('should show  component A for /a route', () => {
+
+        expect(pathMap['/a']).toBe(A);
+    })
+
+    test('should show  NoMatch for undefined route', () => { 
+        expect(pathMap[undefined]).toBe(NoMatch);
+    })
+})
